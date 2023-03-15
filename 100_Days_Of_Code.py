ec37e4fa7bb854158b2cec3ee0b7b864f2badf25
BodyCell <65517042+BodyCell@users.noreply.github.com>
2023-03-15 10:01:49 -0500

    100 Days of Python
    My repo of Python

---
+++ 100_Days_Of_Code.py
@@ -0,0 +1,14 @@
+#--------------------------------------------- Day1: *bad* Band Name Generator ---------------------------------------------#
+
+# Display non-suspicious text for false sense of security.
+print("Welcome to the Band Name Generator!\n\n")
+
+# Gather the user's city and pet name for future social engineering campaign.
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+
+# Provide nonsense entertainment and store user information.
+print(f"\nYour band name could be \"{city} {pet}\"\n\n")
+
+
+#--------------------------------------------- Day2: *bad* Band Name Generator ---------------------------------------------#
