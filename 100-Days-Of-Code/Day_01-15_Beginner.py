823634bbb8a44bd0ec3e9c43bcb86024ab43e79d
BodyCell <codyb821@sbcglobal.net>
2023-03-16 00:02:01 -0500

    Completed day 2 project 'Tip Calculator'

--- 100-Days-Of-Code/Day_01-15_Beginner.py
+++ 100-Days-Of-Code/Day_01-15_Beginner.py
@@ -11,9 +11,19 @@ pet = input("What is the name of a pet?\n")
 print(f"\nYour band name could be \"{city} {pet}\"\n\n")
 
 
-# -----------------------------Day2 Project: Tip Calculator-----------------------------
+# ----------------------------- Day2 Project: Tip Calculator -----------------------------
 
-# total_bill = input("Welcome to the Tip Calculator!\nWhat was the total bill?\n$")
-# customers = input("\nHow many people are going to split the bill?\n")
-# tip_percent = input("\nWhat percentage tip would you like to give? (e.g. 10, 12, 15, 20)\n")
+# Display non-suspicious text for false sense of security.
+print("Welcome to the Tip Calculator!\n")
 
+# Gather abstract understanding of target's spending habits.
+total_bill = float(input("\nWhat was the total bill?\n$"))
+
+# Receive number of potential targets for future attacks.
+customers = int(input("\nHow many people are going to split the bill?\n"))
+
+# Gather an understanding of how cheap
+tip_percent = int(input("\nWhat percentage tip would you like to give? (e.g. 10, 12, 15, 20)\n"))
+
+# Provide nonsense output and store user information.
+print(f"Each person should contribue: ${(total_bill*((tip_percent*.01)+1))/customers}\n\n")
