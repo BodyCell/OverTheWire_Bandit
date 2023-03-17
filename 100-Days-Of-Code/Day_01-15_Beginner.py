cc3765ef96087f216aff315b7fdb76d9f2c84e27
BodyCell <codyb821@sbcglobal.net>
2023-03-17 03:05:35 -0500

    Added Day 6 project

--- 100-Days-Of-Code/Day_01-15_Beginner.py
+++ 100-Days-Of-Code/Day_01-15_Beginner.py
@@ -171,5 +171,25 @@ def PasswordGenerator():
 
 # PasswordGenerator()
 
-#----------------------------- Day6 Project: -----------------------------#
+#----------------------------- Day6 Project: Reeborg Maze -----------------------------#
+#URL to maze: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if wall_on_right():
+#         if front_is_clear():
+#             move()
+#         else:
+#             turn_left()
+#     else:
+#         turn_right()
+#         move()
